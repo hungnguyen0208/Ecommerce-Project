e3bff1dbbfae7b6593e4dc55efe964668e80a0bd
Hung Nguyen <hungnguyen.k51.ftu@gmail.com>
2024-03-18 09:56:17 +0700

    Initial Commit

---
+++ EcomWebParent/EcomBackend/src/main/java/com/maxnguyen/admin/EcomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.maxnguyen.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomBackendApplication.class, args);
+	}
+
+}
---
+++ EcomWebParent/EcomFrontend/src/main/java/com/maxnguyen/site/EcomFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.maxnguyen.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomFrontendApplication.class, args);
+	}
+
+}
